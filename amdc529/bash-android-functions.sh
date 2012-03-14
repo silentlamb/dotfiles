b8b9485b7d783b57f206d0acc267c832adf16568
Marcin Glinski <marcin@marcinglinski.pl>
2012-03-14 16:46:35 +0100

    Added android ascii-art to the droid-help function

--- amdc529/bash-android-functions.sh
+++ amdc529/bash-android-functions.sh
@@ -5,26 +5,38 @@ source ~/dotfiles/amdc529/samsung-bash-functions.sh
 
 droid-help ()
 {
-    echo 
-    echo "            List of functions (see source file for usage help)            "
-    echo "              * - not implemented yet                                     "
-    echo 
-    echo "=== Game backup =========================================================="
-    echo " *droid-backup-game            create backup for given game from device   "
-    echo " *droid-install-game           install given game to the device           "
-    echo " *droid-create-config          create config for given game               "
+cat << EOF
+      \`\,.---.,/'
+       .'     \`.           List of functions
+      /  o   o  \
+  __ :___________: __      See source file for usage help
+ :  :|"""""""""""|:  :
+ |  ||           ||  |       * - not implemented yet
+ |  ||           ||  |
+ :  :|           |:  :
+  "" |           | ""
+     \`-.  .-.  .-'                Author:   Marcin Glinski
+       |  | |  |                  Modified: 2012-03-14
+       :  : :  :
+       \`--' \`--'
+
+=== Game backup ==========================================================
+ *droid-backup-game            create backup for given game from device
+ *droid-install-game           install given game to the device
+ *droid-create-config          create config for given game
+
+=== Android/adb related functions ========================================
+  droid-fetch-apk              fetch apk from the device & unpack to /tmp
+  droid-list-apk               display list of apks on the device
+
+EOF
+    samsung-droid-help
     echo
-    echo "=== Android/adb related functions ========================================"
-    echo "  droid-fetch-apk              fetch apk from the device & unpack to /tmp "
-    echo "  droid-list-apk               display list of apks on the device         "
-    echo 
-    samsung-droid-help()
-    echo 
 }
 
 
 # List apks on the device
-# 
+#
 #   Usage:
 #   -w    wait for device (default)
 #   -W    don't wait for device
