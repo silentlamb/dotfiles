2647091649c8ee30539cc4591a98b7e89f77d7d9
Marcin Glinski <marcin@marcinglinski.pl>
2012-03-30 17:53:48 +0200

    Changed ls alias to display directories first, removed unused function

--- bash-aliases.sh
+++ bash-aliases.sh
@@ -1,10 +1,12 @@
+ALIAS_LS_OPTIONS="--group-directories-first"
+
+
 ### Add color to various commands
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls --color=auto'
+    ALIAS_LS_OPTIONS="${ALIAS_LS_OPTIONS} --color=auto"
     alias dir='dir --color=auto'
     alias vdir='vdir --color=auto'
-
     alias grep='grep --color=auto'
     alias fgrep='fgrep --color=auto'
     alias egrep='egrep --color=auto'
@@ -13,16 +15,16 @@ if [ -x /usr/bin/dircolors ]; then
 fi
 
 # 
-alias l='ls -lF'
-alias ll='ls -alF'
+alias ls="ls ${ALIAS_LS_OPTIONS}"
+alias l='ls -lF --group-directories-first'
+alias ll='ls -alF --group-directories-first'
 alias f='find . '
 
 alias push="adb -d push"
 alias pull="adb -d pull"
 
+
 ### Parametrized
-function pull-apk() {       adb -d pull /data/app/$@; }
 function lapk() {           adb -d shell ls /data/app; }
 function get-last-apk() {   echo $(adb shell ls -l /data/app | cut -b39- | sort -r | head -n1 | awk '{ print $3 }');   }
-
 
