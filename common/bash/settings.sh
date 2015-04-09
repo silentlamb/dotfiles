0459e63311531304ec709c2466c7f31b082208f9
Marcin Glinski <marcin@marcinglinski.pl>
2015-04-09 10:14:22 +0200

    Coloring and paths added to the common/bash/settings

--- common/bash/settings.sh
+++ common/bash/settings.sh
@@ -17,3 +17,54 @@ fi
 ### make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
 
+###
+### Programming paths and variables
+###
+CPATH=${HOME}/Libs/Cpp/include
+CPLUS_INCLUDE_PATH=${PATH}
+LIBRARY_PATH=${HOME}/Libs/Cpp/lib
+
+# Less/Most
+MANPATH=/usr/local/man:/usr/local/share/man:/usr/share/man:/usr/man
+# Colors
+default=$(tput sgr0)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+purple=$(tput setaf 5)
+orange=$(tput setaf 9)
+
+# Less colors for man pages
+export PAGER=less
+export LESS="--RAW-CONTROL-CHARS"
+# export LESS_TERMCAP_mb=$'\E[01;31m'      # begin blinking
+# export LESS_TERMCAP_md=$'\E[01;31m'      # begin bold
+# export LESS_TERMCAP_me=$'\E[0m'          # end mode
+# export LESS_TERMCAP_se=$'\E[0m'          # end standout-mode                 
+# export LESS_TERMCAP_so=$'\E[01;44;33m'   # begin standout-mode - info box                              
+# export LESS_TERMCAP_ue=$'\E[0m'          # end underline
+# export LESS_TERMCAP_us=$'\E[01;32m'      # begin underline
+
+# export LESS_TERMCAP_mb=$(printf '\e[01;31m') # enter blinking mode
+# export LESS_TERMCAP_md=$(printf '\e[01;38;5;75m') # enter double-bright mode
+# export LESS_TERMCAP_me=$(printf '\e[0m') # turn off all appearance modes (mb, md, so, us)
+# export LESS_TERMCAP_se=$(printf '\e[0m') # leave standout mode
+# export LESS_TERMCAP_so=$(printf '\e[01;33m') # enter standout mode
+# export LESS_TERMCAP_ue=$(printf '\e[0m') # leave underline mode
+# export LESS_TERMCAP_us=$(printf '\e[04;38;5;200m') # enter underline mode
+
+# export LESS_TERMCAP_mb=$(printf '\e[01;31m') # enter blinking mode – red
+# export LESS_TERMCAP_md=$(printf '\e[01;35m') # enter double-bright mode – bold, magenta
+# export LESS_TERMCAP_me=$(printf '\e[0m') # turn off all appearance modes (mb, md, so, us)
+# export LESS_TERMCAP_se=$(printf '\e[0m') # leave standout mode
+# export LESS_TERMCAP_so=$(printf '\e[01;33m') # enter standout mode – yellow
+# export LESS_TERMCAP_ue=$(printf '\e[0m') # leave underline mode
+# export LESS_TERMCAP_us=$(printf '\e[04;36m') # enter underline mode – cyan 
+
+export LESS_TERMCAP_mb=$'\E[01;31m'       # begin blinking
+export LESS_TERMCAP_md=$'\E[01;38;5;74m'  # begin bold
+export LESS_TERMCAP_so=$'\E[38;5;246m'    # begin standout-mode - info box
+export LESS_TERMCAP_us=$'\E[04;38;5;146m' # begin underline
+export LESS_TERMCAP_me=$'\E[0m'           # end mode
+export LESS_TERMCAP_se=$'\E[0m'           # end standout-mode
+export LESS_TERMCAP_ue=$'\E[0m'           # end underline
+
