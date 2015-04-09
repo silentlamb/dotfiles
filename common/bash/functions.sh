83a240cab1cab089e11f8f3ecec151abc693aa48
Marcin Glinski <marcin@marcinglinski.pl>
2015-04-09 10:09:17 +0200

    Added `wtle` bash function to run command in the background.
    The function make use of `nohup` and `disown`.

--- common/bash/functions.sh
+++ common/bash/functions.sh
@@ -3,3 +3,10 @@ function powerline_update_ps1 ()
 	export PS1="$(~/dotfiles/external/powerline-shell/powerline-shell.py $? 2> /dev/null)"
 }
 
+function wtle ()
+{
+	nohup $@ > /dev/null 2>&1 &
+	disown
+}
+complete -F _command wtle
+
